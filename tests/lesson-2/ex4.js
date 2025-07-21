72656e8354469b39ff94e7315f1abff971153762
Tien <tienton1603@gmail.com>
2025-07-21 09:11:10 +0700

    chore: fix file lesson-2 ex4.js file

--- tests/lesson-2/ex4.js
+++ tests/lesson-2/ex4.js
@@ -5,7 +5,7 @@ let cannang_lytuong = (sole_chieucao *9/10).toFixed(2);
 let cannang_max = (sole_chieucao).toFixed(2);
 let cannang_min = (sole_chieucao*8/10).toFixed(2);
 
-console.log(sole_chieucao);
-console.log("Cân nặng lý tưởng của bạn là:" + cannang_lytuong);
-console.log("Cân nặng tối đa của bạn là:"  + cannang_max);
-console.log("Cân nặng tối thiểu của bạn là:"  + cannang_min);
+
+console.log("Cân nặng lý tưởng của bạn là:" + cannang_lytuong + "/ " + "Cân nặng tối đa của bạn là:" + cannang_max + "/ " + "Cân nặng tối thiểu của bạn là:"  + cannang_min);
+//console.log("Cân nặng tối đa của bạn là:"  + cannang_max);
+//console.log("Cân nặng tối thiểu của bạn là:"  + cannang_min);
